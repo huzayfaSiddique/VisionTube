2287b968a0408680c70c69991c9d56bcd2852ea8
huzayfaSiddique <huzayfasiddique505@gmail.com>
2026-07-21 14:20:10 +0500

    Connect to mongodb altas

---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+async function connectDB(){
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB connected!! DB host: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("Error:", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
