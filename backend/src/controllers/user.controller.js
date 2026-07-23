b199b6fc89867e6751bfb628da965d607c1cf084
huzayfaSiddique <huzayfasiddique505@gmail.com>
2026-07-23 20:45:07 +0500

    feat: implement subscription model

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
-import { User } from "../db/user.models.js";
+import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 const registerUser = asyncHandler(async(req,res) =>{
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import jwt from "jsonwebtoken"  
-import {User} from "../db/user.models.js"  
+import {User} from "../models/user.models.js"  
 
 export const verifyJWT = asyncHandler(async(req,_,next)=>{
     try{
