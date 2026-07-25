2e0938b5aab7a64f3c8b76c2b933de3d36355c2a
huzayfaSiddique <huzayfasiddique505@gmail.com>
2026-07-25 10:09:35 +0500

    feat: implement Comment, Like, Playlist, and Tweet mongoose models

---
+++ backend/src/models/playlist.models.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name:{
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    isPublic: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
